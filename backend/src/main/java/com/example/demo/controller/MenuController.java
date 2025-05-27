e57f43a63a0d77455ff7f67b975f69593391d468
haradayuuta <mayutokuyuta@gmail.com>
2025-05-27 14:42:35 +0900

    🔥 getMapping("/menu")の削除

--- backend/src/main/java/com/example/demo/controller/MenuController.java
+++ backend/src/main/java/com/example/demo/controller/MenuController.java
@@ -1,13 +1,12 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MenuController {
 
-	@GetMapping("/menu")
-	public String view() {
-		return "menu";
-	}
+//	@GetMapping("/menu")
+//	public String view() {
+//		return "menu";
+//	}
 }
