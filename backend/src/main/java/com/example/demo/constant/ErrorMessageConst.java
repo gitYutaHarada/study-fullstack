fb42442ebeb358bce8e6a29ba5139f992ef84119
haradayuuta <mayutokuyuta@gmail.com>
2025-05-31 20:05:19 +0900

    ✨ エラーメッセージをプロパティファイルで管理

---
+++ backend/src/main/java/com/example/demo/constant/ErrorMessageConst.java
@@ -0,0 +1,7 @@
+package com.example.demo.constant;
+
+public class ErrorMessageConst {
+
+	//ログイン失敗用
+	public static final String LOGIN_WRONG_INPUT = "login.wrongInput";
+}
--- backend/src/main/java/com/example/demo/controller/LoginController.java
+++ backend/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,12 +1,15 @@
 package com.example.demo.controller;
 
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.constant.ErrorMessageConst;
 import com.example.demo.dto.LoginRequestDto;
 import com.example.demo.service.user.login.LoginService;
+import com.example.demo.util.AppUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,17 +19,20 @@ public class LoginController {
 
 	private final LoginService service;
 	
+	private final MessageSource messageSource;
+	
 	
 	@PostMapping("/api/login")
 	@ResponseBody
-	public boolean login(@RequestBody LoginRequestDto loginRequestDto) {
+	public String login(@RequestBody LoginRequestDto loginRequestDto) {
 		
 		if(service.isLogin(loginRequestDto.getUserId(), loginRequestDto.getPassword())) {
 			System.out.println("true");
-			return true;
+			return "true";
 		} else {
 			System.out.println("false");
-			return false;
+			String errMsg = AppUtil.getMessage(messageSource, ErrorMessageConst.LOGIN_WRONG_INPUT);
+			return errMsg;
 		}
 	}
 }
