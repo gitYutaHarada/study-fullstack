c11d0bedde65b8123c3edc109b1985b8fc9425ec
haradayuuta <mayutokuyuta@gmail.com>
2025-06-02 19:05:38 +0900

    🎨 signupの設定

--- frontend/src/hooks/api/useSignup.jsx
+++ frontend/src/hooks/api/useSignup.jsx
@@ -3,23 +3,24 @@
 import { useState } from "react";
 
 export const useSginup = (userName, password) => {
-    const [isSignup, setIsSignup] = useState(false);
+    const [isSignup, setIsSignup] = useState(null);
 
-    const handleSginup = async(e) => {
-        e.preventDefault();
+    const handleSginup = async (e) => {
+      e.preventDefault();
+      console.log(isSignup);
 
-        try{
-            const res = await fetch("api/signup", {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({userName, password}),
-            });
-            const response = await res.json();
-        } catch (err){
-            console.log(err);
-
-        }
-    }
+      try {
+        const res = await fetch("/api/signup", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ userName, password }),
+        });
+        const response = await res.json();
+        setIsSignup(response.isSignup);
+      } catch (err) {
+        console.log(err);
+      }
+    };
     return { isSignup, handleSginup};
 }
 
